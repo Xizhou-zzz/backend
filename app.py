6369410e3e547bc8d78c6f098606ec0e7391897d
tjw <1485891941@qq.com>
2023-07-07 10:06:59 +0800

    前后端传输部分

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, send_from_directory, redirect, url_for
+from flask import Flask, render_template, send_from_directory, redirect, url_for, request
 
 app = Flask(__name__)
 
@@ -17,6 +17,19 @@ def serve_static(path):
 @app.route('/')
 def index():
     return redirect('index.html')
+
+
+@app.route('/api/login', methods=['POST'])
+def process_data():
+    # 获取前端传输的数据
+    data = request.json  # 假设数据是以JSON格式传输
+    # 或者使用 request.form 来获取表单数据
+
+    # 在这里对接收到的数据进行处理
+    # ...
+    print(data)
+    # 返回响应给前端
+    return 'Data received and processed successfully'
 
 
 if __name__ == '__main__':
