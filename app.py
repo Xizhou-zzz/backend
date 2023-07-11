babd5e244ba8f960b0a767b4d6ffda09c9430181
Xizhou-zzz <1158398445@qq.com>
2023-07-11 08:35:38 +0800

    随便改改

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, send_from_directory, redirect, abort, jsonify, request
+from flask import Flask, jsonify, request
 from flask_cors import CORS
 from controller import DBcontroller
 import pandas as pd
