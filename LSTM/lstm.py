15da25fb28c82077ede5244c4e46e3a2c8e584a4
Xizhou-zzz <1158398445@qq.com>
2023-07-12 16:46:32 +0800

    test

--- LSTM/lstm.py
+++ LSTM/lstm.py
@@ -1,28 +1,7 @@
 import numpy as np
 import pandas as pd
-from tensorflow import keras
-from sklearn.preprocessing import MinMaxScaler
 from tensorflow.keras.models import Sequential
 from tensorflow.keras.layers import LSTM, Dropout, Dense
-from tensorflow.keras.models import load_model
-from tensorflow.keras.callbacks import ModelCheckpoint
-
-# 构建与训练模型的代码
-
-# 创建一个新的模型对象
-model = Sequential([
-    LSTM(units=256, input_shape=(1, 9), return_sequences=True),
-    Dropout(0.4),
-    LSTM(units=256, return_sequences=True),
-    Dropout(0.3),
-    LSTM(units=128, return_sequences=True),
-    LSTM(units=32),
-    Dense(1)
-])
-model.compile(optimizer='adam', loss='mse')
-
-# 加载训练过程中保存的最佳模型权重
-model.load_weights('best_model.hdf5')
 
 
 def create_df(t1, t2, hum, wind_speed, weather_code, is_holiday, is_weekend, season, hour):
@@ -49,24 +28,37 @@ def create_df(t1, t2, hum, wind_speed, weather_code, is_holiday, is_weekend, sea
                'season': float(season),
                'hour': (float(hour) - hour_min) / (hour_max - hour_min)}
     df.loc[len(df)] = new_row
+    df = np.array(df)
+    df = np.reshape(df, (1, 1, 9))
+    df = df.astype(np.float32)
     return df
 
 
-tempDf = create_df('3.0', '2.0', '93.0', '6.0', '3.0', '0.0', '1.0', '3.0', '0.0')
+def predict(path, t1, t2, hum, wind_speed, weather_code, is_holiday, is_weekend, season, hour):
+    max_val = 7860
+    min_val = 0
+    model = Sequential([
+        LSTM(units=256, input_shape=(1, 9), return_sequences=True),
+        Dropout(0.4),
+        LSTM(units=256, return_sequences=True),
+        Dropout(0.3),
+        LSTM(units=128, return_sequences=True),
+        LSTM(units=32),
+        Dense(1)
+    ])
+    model.compile(optimizer='adam', loss='mse')
 
-# 调整输入数据的形状
-tempDf = np.array(tempDf)
-tempDf = np.reshape(tempDf, (1, 1, 9))
+    # 加载训练过程中保存的最佳模型权重
+    model.load_weights(path)
+    df = create_df(t1, t2, hum, wind_speed, weather_code, is_holiday, is_weekend, season, hour)
+    predictions = model.predict(df, verbose=1)
+    predictions = predictions*(max_val-min_val)+min_val
+    return predictions
 
-# 将数据类型转换为float32
-tempDf = tempDf.astype(np.float32)
 
-max_val = 7860
-min_val = 0
-# 进行预测
-predictions = model.predict(tempDf, verbose=1)
+def get_predictions(df):
+    data_to_draw = df
+    return data_to_draw
 
-predictions = predictions*(max_val-min_val)+min_val
+# tempDf = create_df('3.0', '2.0', '93.0', '6.0', '3.0', '0.0', '1.0', '3.0', '0.0')
 
-# 输出预测结果
-print(predictions)
--- testForData.py
+++ testForData.py
@@ -1,25 +1,62 @@
 from controller import DBcontroller
-import pandas as pd
+from LSTM import lstm
+
+data_date = '2015/7/1'
 
 db = DBcontroller.Database()
-df = db.select('bikemessage', condition='userid < 10000 and userid > 9900')   # 调用查询方法获取数据（返回一个DataFrame）
-# new_df = new_df.rename(columns={'orderid': 'name', 'userid': 'value'})
-df['start_time'] = pd.to_datetime(df['start_time'])
-df['hour'] = df['start_time'].dt.hour
-# 创建时间段的标签
-time_labels = ['0:00-0:59', '1:00-1:59', '2:00-2:59', '3:00-3:59', '4:00-4:59', '5:00-5:59', '6:00-6:59', '7:00-7:59',
-               '8:00-8:59', '9:00-9:59', '10:00-10:59', '11:00-11:59', '12:00-12:59', '13:00-13:59', '14:00-14:59',
-               '15:00-15:59', '16:00-16:59', '17:00-17:59', '18:00-18:59', '19:00-19:59', '20:00-20:59', '21:00-21:59',
-               '22:00-22:59', '23:00-23:59']
+df = db.select('predict', condition=f'date = { data_date }')
 
-# 使用cut()函数对时间进行分段，并计算每个时段的数量
-time_count = pd.cut(df['hour'], bins=range(0, 25), labels=time_labels, include_lowest=True).value_counts().sort_index().reset_index()
-time_count.columns = ['time_range', 'count']
-# 将结果保存为DataFrame数据流
-time_count_stream = time_count.to_json(orient='records')
+data_to_draw = lstm.get_predictions(df)
 
-# 打印结果
-print(time_count)
+print(data_to_draw)
+# return jsonify(data_to_draw)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# db = DBcontroller.Database()
+# df = db.select('bikemessage', condition='userid < 10000 and userid > 9900')   # 调用查询方法获取数据（返回一个DataFrame）
+# # new_df = new_df.rename(columns={'orderid': 'name', 'userid': 'value'})
+# df['start_time'] = pd.to_datetime(df['start_time'])
+# df['hour'] = df['start_time'].dt.hour
+# # 创建时间段的标签
+# time_labels = ['0:00-0:59', '1:00-1:59', '2:00-2:59', '3:00-3:59', '4:00-4:59', '5:00-5:59', '6:00-6:59', '7:00-7:59',
+#                '8:00-8:59', '9:00-9:59', '10:00-10:59', '11:00-11:59', '12:00-12:59', '13:00-13:59', '14:00-14:59',
+#                '15:00-15:59', '16:00-16:59', '17:00-17:59', '18:00-18:59', '19:00-19:59', '20:00-20:59', '21:00-21:59',
+#                '22:00-22:59', '23:00-23:59']
+#
+# # 使用cut()函数对时间进行分段，并计算每个时段的数量
+# time_count = pd.cut(df['hour'], bins=range(0, 25), labels=time_labels, include_lowest=True).value_counts().sort_index().reset_index()
+# time_count.columns = ['time_range', 'count']
+# # 将结果保存为DataFrame数据流
+# time_count_stream = time_count.to_json(orient='records')
+#
+# # 打印结果
+# print(time_count)
 # print(data)
 
 
